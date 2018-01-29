730cfa26fdb66897a91d3d581ad62f2932571f3f
Yuval Langer <yuval.langer@gmail.com>
2018-01-29 14:34:39 +0200

    Falling back to use-package due to some nasty bug. Something to do with removal of :loader keyword, maybe?

--- init.el
+++ init.el
@@ -1,15 +1,32 @@
+
+;; start emacs server
+;; (from https://github.com/edvorg/emacs-configs/blob/master/init-real.el)
+
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+;; setup elpa addresses
+
 (require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives (cons "gnu" "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives (cons "melpa-bleeding-edge" "https://melpa.org/packages/"))
-(add-to-list 'package-archives (cons "melpa-stable" "https://stable.melpa.org/packages/"))
+
+'(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives ("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives ("melpa-bleeding-edge" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives ("melpa-stable" . "https://stable.melpa.org/packages/"))
+
 (package-initialize)
 
 ;; Bootstrap `use-package'
+
 (unless (package-installed-p 'use-package)
         (package-refresh-contents)
         (package-install 'use-package))
 
-(use-package req-package :ensure t)
+
+(require 'use-package)
+
+(setq use-package-always-ensure t)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
